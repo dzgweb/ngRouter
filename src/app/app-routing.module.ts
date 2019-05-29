b4ac92dfccc091558a09a82b54b447585cd2a6e2
Sergey Dziadevich <sergey_dziadevich@epam.com>
2019-05-29 18:05:16 +0300

    set Page Title

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,11 +12,13 @@ import { AuthGuard, CustomPreloadingStrategyService } from './core';
 const routes: Routes = [
   {
     path: 'about',
-    component: AboutComponent
+    component: AboutComponent,
+    data: { title: 'About' }
   },
   { 
     path: 'login', 
-    component: LoginComponent 
+    component: LoginComponent,
+    data: { title: 'Login' }
   },
   {
     path: 'messages',
@@ -26,12 +28,16 @@ const routes: Routes = [
   {
     path: 'admin',
     canLoad: [AuthGuard],
-    loadChildren: './admin/admin.module#AdminModule'
+    loadChildren: './admin/admin.module#AdminModule',
+    data: { title: 'Admin' }
   },
   {
     path: 'users',
     loadChildren: './users/users.module#UsersModule',
-    data: { preload: false }
+    data: {
+      preload: true,
+      title: 'Users'
+    }
   },
 
   {
@@ -43,7 +49,8 @@ const routes: Routes = [
     // The router will match this route if the URL requested
     // doesn't match any paths for routes defined in our configuration
     path: '**',
-    component: PathNotFoundComponent
+    component: PathNotFoundComponent,
+    data: { title: 'Page Not Found' }
   }
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router, NavigationEnd } from '@angular/router';
+
+// rxjs
+import { Subscription } from 'rxjs';
+import { filter, map, switchMap } from 'rxjs/operators';
 
 import { MessagesService, SpinnerService } from './core';
 
@@ -8,13 +13,26 @@ import { MessagesService, SpinnerService } from './core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  private sub: Subscription;
+
   title = 'ngTaskManager';
+
   constructor(
     private router: Router,
     public messagesService: MessagesService,
-    public spinnerService: SpinnerService
+    public spinnerService: SpinnerService,
+    private titleService: Title
   ) { }
+
+
+  ngOnInit() {
+    this.setPageTitles();
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 
   onActivate($event) {
     console.log('Activated Component', $event);
@@ -28,4 +46,23 @@ export class AppComponent {
     this.router.navigate([{ outlets: { messages: ['messages'] } }]);
     this.messagesService.isDisplayed = true;
   }
+  
+  private setPageTitles() {
+    this.sub = this.router.events
+      .pipe(        
+        filter(event => event instanceof NavigationEnd),
+        map(() => this.router.routerState.root),
+        map(route => {
+          while (route.firstChild) {
+            route = route.firstChild;
+          }
+          return route;
+        }),
+        filter(route => route.outlet === 'primary'),
+        switchMap(route => route.data)
+      )
+      .subscribe(
+         data => this.titleService.setTitle(data['title'])
+      );
+  } 
 }
