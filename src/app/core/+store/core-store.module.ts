a56d38c361a47a90d54bde63cce7fba35b164c12
Sergey Dziadevich <sergey_dziadevich@epam.com>
2019-06-24 11:11:22 +0300

    19. Router State

--- src/app/core/+store/core-store.module.ts
+++ src/app/core/+store/core-store.module.ts
@@ -6,15 +6,18 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from './../../../environments/environment';
-
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { RouterStateSerializerProvider, routerReducers } from './router';
 
 @NgModule({
     imports: [
       CommonModule,
-      StoreModule.forRoot({}),
+      StoreModule.forRoot(routerReducers),
+      StoreRouterConnectingModule.forRoot(),
       EffectsModule.forRoot([]),
       // Instrumentation must be imported after importing StoreModule (config is optional) 
       !environment.production ? StoreDevtoolsModule.instrument() : [],
-    ]
+    ],
+    providers: [RouterStateSerializerProvider]
   })
   export class CoreStoreModule { }
